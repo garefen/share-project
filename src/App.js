da2d12486b3fe085ef9ca35f6ae86a4faae1ed40
Garefen <gabrielbsarti@gmail.com>
2020-07-20 20:33:44 -0300

    added component text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
 
+import Text from './components/Text';
+
 function App() {
   return (
     <div className="App">
-      
+      <Text label={'label'} />
     </div>
   );
 }
